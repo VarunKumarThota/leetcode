307900686b297c6fb22ffb56c6dc97d4db740e64
Varun Kumar Thota <100424217+VarunKumarThota@users.noreply.github.com>
2025-08-25 22:44:29 +0530

    Update and rename Longest Palindromic Substring.java to 44.Longest Palindromic Substring.java
    for dp solution see gfg or striver

--- 44.Longest Palindromic Substring.java
+++ 44.Longest Palindromic Substring.java
@@ -36,10 +36,33 @@ public class Main
 	}
 }
 
+//2. using tabulation DP
 
-// 2. second approach - using dp.
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int n = s.length();
+        int m = p.length();
+        boolean[][] dp = new boolean[n+1][m+1];
 
-//using DP
+        dp[0][0] = true;
+
+        for(int j = 1;j <= m;j++){
+            if(p.charAt(j - 1) == '*') dp[0][j] = dp[0][j - 1];
+        }
+
+        for(int i = 1;i < n + 1;i++){
+            for(int j = 1; j < m + 1;j++){
+                if(s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?')
+                dp[i][j] = dp[i - 1][j - 1];
+                if( p.charAt(j - 1) == '*')
+                dp[i][j]  = dp[i - 1][j] || dp[i][j - 1];
+            }
+        }
+        return dp[n][m];
+    }
+}
+
+// 3. second approach - using memorization.
 class Solution {
     public String longestPalindrome(String s) {
         int n = s.length();
