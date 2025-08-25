2bb7ad653db5228ac70d299cc182c644264f51f8
Varun Kumar Thota <100424217+VarunKumarThota@users.noreply.github.com>
2025-08-25 14:10:00 +0530

    5.Update Longest Palindromic Substring.java
    2. second approach - using dp.

--- Longest Palindromic Substring.java
+++ Longest Palindromic Substring.java
@@ -6,7 +6,7 @@ C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS
 Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
-
+// 1. first Approach - looping over and taking element as center of palindrome.
 public class Main
 {
     public static String longestpalidromic(String s){
@@ -34,4 +34,42 @@ public class Main
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
+}
+
+
+// 2. second approach - using dp.
+
+//using DP
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int start = 0;
+        int maxLen = 1;
+        for(int i = 0;i < n;i++){
+            dp[i][i] = true;
+        }
+        for(int i = 0; i < n - 1; i++){
+            if(s.charAt(i)==s.charAt(i+1)){
+                dp[i][i+1] = true;
+                if(maxLen == 1){
+                maxLen = 2;
+                start = i;
+                }
+            }
+        }
+        for(int len = 3;len <= n;len++){
+            for(int i = 0;i <= n - len;i++){
+                int j = i + len - 1;
+                if( s.charAt(i)==s.charAt(j) && dp[i+1][j-1]){
+                    dp[i][j] = true;
+                    if(len > maxLen){
+                        maxLen = len;
+                        start = i;
+                    }
+                }
+            }
+        }
+        return s.substring(start,start + maxLen);
+    }
 }
